b1e23d16b30af210c2d16bf985aca0fc6b32ecfe
ElenaLj <elenaljubijankic@gmail.com>
2021-11-19 15:06:28 +0100

    init js e array oggetti

---
+++ our-team-base/script.js
@@ -0,0 +1,43 @@
+// alert("Hello World!");
+
+// crea un array di oggetti in cui ogni oggetto rappresenta un membro del team. Ogni membro deve avere le informazioni necessarie per stampare: nome, ruolo e foto;
+
+// creo array di oggetti
+const team = [
+    {
+        "nome": "Wayne Barnett",
+        "ruolo": "Founder & CEO",
+        "foto": "img/wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        "nome": "Angela Caroll",
+        "ruolo": "Chief Editor",
+        "foto": "img/angela-caroll-chief-editor.jpg"
+    },
+    {
+        "nome": "Walter Gordon",
+        "ruolo": "Office Manager",
+        "foto": "img/walter-gordon-office-manager.jpg"
+    },
+    {
+        "nome": "Angela Lopez",
+        "ruolo": "Social Media Manager",
+        "foto": "img/angela-lopez-social-media-manager.jpg"
+    },
+    {
+        "nome": "Scott Estrada",
+        "ruolo": "Developer",
+        "foto": "img/scott-estrada-developer.jpg"
+    },
+    {
+        "nome": "Barbara Ramos",
+        "ruolo": "Graphic Designer",
+        "foto": "img/barbara-ramos-graphic-designer.jpg"
+    },
+];
+
+// controllo in console.log
+console.log(team);
+
+// vado a selezionare il tag html di punta
+let teamCard = document.querySelector(".team-card");
